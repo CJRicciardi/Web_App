2303770be2bd114c293ecc317da004403738fd4f
CJ Ricciardi <ricciardistg@gmail.com>
2020-03-24 17:34:47 -0500

    Twitter API added.

---
+++ services/twitter_service.py
@@ -0,0 +1,24 @@
+# web_app/services/twitter_service.py
+
+import os
+from dotenv import load_dotenv
+import tweepy
+
+load_dotenv()
+
+consumer_key = os.getenv("TWITTER_API_KEY", default="OOPS")
+consumer_secret = os.getenv("TWITTER_API_SECRET", default="OOPS")
+
+access_token = os.getenv("TWITTER_ACCESS_TOKEN", default="OOPS")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET", default="OOPS")
+
+# Authenticate
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
