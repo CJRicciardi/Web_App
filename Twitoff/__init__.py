655fc1b5aeef57b48df4cc2928f9d28492662f4c
CJ Ricciardi <ricciardistg@gmail.com>
2020-03-24 06:48:45 -0500

    Use blueprints

--- Twitoff/__init__.py
+++ Twitoff/__init__.py
@@ -2,24 +2,14 @@
 
 from flask import Flask
 
-#from twitoff.routes.home_routes import home_routes
-#from twitoff.routes.book_routes import book_routes
+from Twitoff.routes.home_routes import home_routes
+#from Twitoff.routes.book_routes import book_routes
 
 
 def create_app():
     app = Flask(__name__)
-    #app.register_blueprint(home_routes)
+    app.register_blueprint(home_routes)
     #app.register_blueprint(book_routes)
-
-    @app.route("/")
-    def hello():
-        print('vsited the hello page')
-        return "Hello World!"
-
-    @app.route("/about")
-    def about():
-        print('visited the about page')
-        return "About me"
 
     return app
 
---
+++ Twitoff/routes/home_routes.py
@@ -0,0 +1,15 @@
+# Web_App/Twitoff/routes/home_routes.py
+
+from flask import Blueprint
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def hello():
+    print('vsited the hello page')
+    return "Hello World!"
+
+@home_routes.route("/about")
+def about():
+    print('visited the about page')
+    return "About me"
