3ec850ac0f3dc2d4e0e53a383a819cf27d410c7c
CJ Ricciardi <ricciardistg@gmail.com>
2020-03-24 11:11:44 -0500

    Generated development db.

--- Twitoff/__init__.py
+++ Twitoff/__init__.py
@@ -4,10 +4,17 @@ from flask import Flask
 
 from Twitoff.routes.home_routes import home_routes
 from Twitoff.routes.book_routes import book_routes
-
+from Twitoff.models import db, migrate
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\ricci\\Google Drive\\Colab Notebooks\\Unit_3\\Sprint_3\\Web_App\\Twitoff.db"
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
 
