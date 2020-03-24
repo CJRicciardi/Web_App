466fc71bb65ed0e852635a3f602b498221ba2a54
CJ Ricciardi <ricciardistg@gmail.com>
2020-03-24 07:06:15 -0500

    Added books page

--- Twitoff/routes/book_routes.py
+++ Twitoff/routes/book_routes.py
@@ -1,10 +1,20 @@
 # Web_App/Twitoff/routes/book_routes.py
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
+def list_books():
+    print('requested the books in json format')
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"},
+    ]  # todo: get from the database
+    return jsonify(books)
+
+@book_routes.route("/books")
 def books():
     print('visited the books page')
     books = [
@@ -12,4 +22,4 @@ def books():
         {"id": 2, "title": "Book 2"},
         {"id": 3, "title": "Book 3"},
     ]  # todo: get from the database
-    return jsonify(books)
+    return render_template("books.html")
